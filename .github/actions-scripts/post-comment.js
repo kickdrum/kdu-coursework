66500f73c56342286e89a5f024b13e5581fc2b07
Sagun Sangwan <sagun.sangwan@kickdrumtech.com>
2024-01-08 17:05:33 +0530

    Changed comment

--- .github/actions-scripts/post-comment.js
+++ .github/actions-scripts/post-comment.js
@@ -92,8 +92,6 @@ async function fetchMetricsData() {
       params,
       ...axiosConfig,
     });
-
-    console.log("RES", await response.getJSON());
     const metrics = response.data.measures.map((metric) => ({
       key: metric.metric,
       value: metric.value,
