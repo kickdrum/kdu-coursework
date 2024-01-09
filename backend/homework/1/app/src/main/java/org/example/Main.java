ed177be97e95e9832b8435a099e121451fd3f79c
Neer Patel <neerpatel25@gmail.com>
2024-01-09 11:40:45 +0530

    second

--- backend/homework/1/app/src/main/java/org/example/Main.java
+++ backend/homework/1/app/src/main/java/org/example/Main.java
@@ -2,26 +2,42 @@ package org.example;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+
+import java.util.Scanner;
+
 public class Main {
     private static final Logger logger = LogManager.getLogger(Main.class);
 
     public static void main(String[] args) {
         StudentReposittory repository = new StudentReposittory(1);
-
-        // Adding a student
-        Student newStudent1 = new Student(1, "NeerPatel", 22, 3);
-        repository.addStudent(newStudent1);
-        logger.fatal("Added student: {}", newStudent1.getName());
-        Student newStudent2 = new Student(1, "pavan", 22, 5);
-        repository.addStudent(newStudent2);
-        logger.fatal("Added student: {}", newStudent2.getName());
-
-
-
-        // Retrieving a student
-        Student retrievedStudent = repository.getStudentById(1);
-        if (retrievedStudent != null) {
-            logger.fatal("Retrieved Student: {}", retrievedStudent.getName());
+        Scanner scn = new Scanner(System.in);
+        System.out.println("Enter Number Student want to Add: ");
+        int n = scn.nextInt();
+        for (int i = 0; i < n; i++) {
+            System.out.println("Enter Id:");
+            int id = scn.nextInt();
+            System.out.println("Enter Name:");
+            String name = scn.next();
+            System.out.println("Enter Age:");
+            int age = scn.nextInt();
+            System.out.println("Enter Grade:");
+            int grade = scn.nextInt();
+            Student newStudent1 = new Student(id, name, age, grade);
+            repository.addStudent(newStudent1);
+            logger.fatal("Added student: {}", newStudent1.getName());
         }
+
+        System.out.println("Enter number details you want Retrive:");
+        int n1= scn.nextInt();
+        for(int i=0;i<n1;i++){
+            System.out.println("Enter the id Which you want retrive:");
+            int v = scn.nextInt();
+            Student retrievedStudent = repository.getStudentById(v);
+            if (retrievedStudent != null) {
+                logger.fatal("Retrieved Student: {}", retrievedStudent.getName());
+            }
+        }
+
+
     }
 }
