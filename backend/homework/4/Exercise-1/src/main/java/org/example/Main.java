eb76ef1ba569876873c54e54458c690d06382700
Neer Patel <neerpatel25@gmail.com>
2024-01-12 00:28:52 +0530

    made more changes

--- backend/homework/4/Exercise-1/src/main/java/org/example/Main.java
+++ backend/homework/4/Exercise-1/src/main/java/org/example/Main.java
@@ -2,13 +2,13 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        StudentUtil student = new StudentUtil();
 
-        int[] SID = {1001, 1002};
+
+        int[] studentid = {1001, 1002};
         char[][] grades = {{'A', 'A', 'A', 'B'}};
-        double[] d=new double[0];
+
         try {
-             d = StudentUtil.calculateGPA(SID, grades);
+             StudentUtil.calculateGPA(studentid, grades);
 
         } catch (MissingGradeException e) {
             throw new InvalidDataException("Invalid data in calculateGPA method.", e);
