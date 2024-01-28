0ef11d7aa9b41e13ebde89de05130425bda4d643
Prashant Shukla <77752155+prashant1shukla@users.noreply.github.com>
2024-01-28 23:53:20 +0530

    Reduced Code Smells

--- backend/assignments/2/src/main/java/com/caching/scheduled/CacheCleaner.java
+++ backend/assignments/2/src/main/java/com/caching/scheduled/CacheCleaner.java
@@ -1,7 +1,6 @@
 package com.caching.scheduled;
 
 import lombok.Data;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cache.Cache;
 import org.springframework.cache.CacheManager;
 import org.springframework.scheduling.annotation.Scheduled;
