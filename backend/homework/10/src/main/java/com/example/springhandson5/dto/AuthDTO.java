083b828aaebfd3190fc8e0c8863498ae6d569be0
Kunal Madan <kunalmadan0611@gmail.com>
2024-01-30 14:40:24 +0530

    Commit went wrong

--- backend/homework/10/src/main/java/com/example/springhandson5/dto/AuthDTO.java
+++
@@ -1,9 +0,0 @@
-package com.example.springhandson5.dto;
-
-import lombok.Data;
-
-@Data
-public class AuthDTO {
-    String username;
-    String password;
-}
--- backend/homework/10/src/main/java/com/example/springhandson5/exception/custom/CustomException.java
+++
@@ -1,7 +0,0 @@
-package com.example.springhandson5.exception.custom;
-
-public class CustomException extends IndexOutOfBoundsException{
-    public CustomException(String message){
-        super(message);
-    }
-}
