e5066113141093693cf699bf5b3e1e20d82082ef
Prashant Shukla <77752155+prashant1shukla@users.noreply.github.com>
2024-02-01 18:02:51 +0530

    Update PrashantJpaApplicationTests.java

--- backend/homework/12/src/test/java/com/example/prashantjpa/PrashantJpaApplicationTests.java
+++ backend/homework/12/src/test/java/com/example/prashantjpa/PrashantJpaApplicationTests.java
@@ -3,7 +3,7 @@ package com.example.prashantjpa;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes=PrashantJpaApplication.class)
 class PrashantJpaApplicationTests {
 
 	@Test
