8c4da18e8fe65f1783e938462a68e542756f89d5
aakash <aakashtyagi232@gmail.com>
2024-02-11 16:49:05 +0530

    initial commit

--- frontend/assignments/1/cypress.config.js
+++ frontend/assignments/1/cypress.config.js
@@ -13,7 +13,7 @@ module.exports = defineConfig({
   // Configuration for end-to-end (e2e) tests
   e2e: {
     // Base URL for your application
-    baseUrl: "http://localhost:5500/assignments/1",
+    baseUrl: "http://google.com",
 
     // Setup Node events for visual regression testing using cypress-visual-regression plugin
     setupNodeEvents(on, config) {
@@ -27,7 +27,7 @@ module.exports = defineConfig({
     PASSWORD: "test",
     USERNAME: "test-user",
     NAME: "test-name",
-    HOME_PAGE_URL: "/home-page/index.html",
+    HOME_PAGE_URL: "/",
     LOGIN_PAGE_URL: "login/index.html",
     REGISTER_PAGE_URL: "/register/index.html",
     TEST_THRESHOLD: 0.35,
