2e331ce18f060e03dfd8e512e647be71ab8829ff
aakash <aakashtyagi232@gmail.com>
2024-02-11 16:10:22 +0530

    fix cypress viewport scaling issue

--- frontend/assignments/1/cypress/support/e2e.js
+++ frontend/assignments/1/cypress/support/e2e.js
@@ -14,7 +14,13 @@
 // ***********************************************************
 
 // Import commands.js using ES2015 syntax:
-import './commands'
+import "./commands";
+
+// Patch fix to globaly change viewports in config
+Cypress.on("viewport:changed", (newValue) => {
+  Cypress.config("viewportWidth", newValue.viewportWidth);
+  Cypress.config("viewportHeight", newValue.viewportHeight);
+});
 
 // Alternatively you can use CommonJS syntax:
 // require('./commands')
