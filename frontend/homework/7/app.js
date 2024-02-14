5d3545927edb650448c6f2aafb37b456692e84f7
prashant1shukla <prashantshukla@gmail.com>
2024-02-14 22:31:33 +0530

    Websockets HW-7

---
+++ frontend/homework/7/app.js
@@ -0,0 +1,38 @@
+const express= require("express");
+const http=require("http");
+const cors=require("cors");
+const socketIo=require("socket.io");
+
+const app=express();
+const server=http.createServer(app);
+const io=new socketIo.Server(server, {
+    cors:{
+        origin: "http://127.0.0.1:5500"
+    }
+});
+app.use(cors())
+
+app.use(express.json())
+
+app.get("/", (req,res)=>{
+    res.json({
+        "msg":"Hello world!"
+    })
+});
+
+io.on("connection", (socket) =>{
+    console.log("Connection created");
+    socket.on("message",(payload)=>{
+        console.log("Payload",payload);
+        io.to(payload.room).except(socket.id).emit("new-message",payload.message)
+    })
+
+    socket.on("roomNo",(payload)=>{
+        console.log("Payload",payload);
+        socket.join(payload);  
+    })
+})  
+
+server.listen(5000, ()=>{
+    console.log("working")
+})
