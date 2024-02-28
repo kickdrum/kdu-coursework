f8639a13865267860bccb13d30a0680bec23f6b7
Prashant Shukla <77752155+prashant1shukla@users.noreply.github.com>
2024-02-28 23:25:47 +0530

    Removed comments

--- frontend/homework/14/src/App.tsx
+++ frontend/homework/14/src/App.tsx
@@ -1,20 +1,7 @@
-// import { BrowserRouter, Routes, Route } from "react-router-dom";
-// import ProductListPage from "./ProductListPage";
-// import ProductDetailPage from "./ProductDetailPage";
-// import { ProductsProvider } from "./ProductsContext";
-
 import { Marketplace } from "./components/Marketplace";
 
 function App() {
   return (
-    // <ProductsProvider>
-    //     <BrowserRouter>
-    //       <Routes>
-    //           <Route path="/" element={<ProductListPage/>} />
-    //           <Route path="/product/:productId" element={<ProductDetailPage/>} />
-    //       </Routes>
-    //     </BrowserRouter>
-    // </ProductsProvider>
     <Marketplace/>
   );
 }
