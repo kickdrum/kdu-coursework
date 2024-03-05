3b375a3c896858b3c05186061407336f76874b89
prithvi206 <prithviraj2062002@gmail.com>
2024-03-05 12:37:23 +0530

    removed folder 12 items

--- frontend/homework/12/todo-list/src/context/SearchContext.ts
+++
@@ -1,9 +0,0 @@
-import { createContext } from "react";
-
-export const SearchContext = createContext<{
-  searchQuery: string;
-  setSearchQuery: (query: string) => void;
-}>({
-  searchQuery: "",
-  setSearchQuery: () => {},
-});
--- frontend/homework/12/todo-list/src/types/ItemProps.ts
+++
@@ -1,4 +0,0 @@
-export interface ItemProps {
-  id: number;
-  name: string;
-}
